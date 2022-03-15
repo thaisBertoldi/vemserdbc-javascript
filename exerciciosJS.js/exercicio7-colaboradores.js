3f0a7b4e6fb8330f3f0de88933a1976995455bb0
thaisbrt <thais_brt@hotmail.com>
2022-03-15 07:53:57 -0300

    Validacoes hora, dia e marcacaoPonto

--- exerciciosJS.js/exercicio7-colaboradores.js
+++ exerciciosJS.js/exercicio7-colaboradores.js
@@ -2,48 +2,6 @@
   +================================================================+
   |                    SISTEMA DE COLABORADORES                    |
   +================================================================+
-
-  Nesse nosso sistema teremos os seguintes modelos de Entidade/Objeto/Classe ("sinônimos"):
-
-  Colaborador {
-    id;
-    nome;
-    marcacoesPonto; (um array de marcações, ex: [ { dia: 22, horas: 9 }, { dia: 23, horas: 7 } ]);
-    
-    marcarPonto( dia, hora );
-  };
-
-  Marcacao {
-      dia; (só um number mesmo, ex: 22 )
-      horas; (também só um number, ex: 9)
-  }
-  
-  Crie também uma classe de 'Validacoes' que terá métodos úteis de validação para usar em várias partes do sistema;
-  Ex: Validacoes { 
-      validaAlgo(valorParaValidar) {
-          // imaginem uma lógica validando aqui
-          return ehValido;
-      }
-  }
-
-  Para utilizar essas validações, 
-
-  Ex: const validador = new Validacoes(); // inicializando um validador (vai ser o mesmo global para o sistema, não precisa instanciar mais de 1)
-  
-  if(validador.validaAlgo('Valor qualquer')) {
-      // só pra exemplificar como utilizar
-  }
-
-  Vamos criar um sistema da seguinte forma;
-  Deve ser apresentado para o usuário um "menu" para ele escolher uma ação a ser realizada;
-
-  As opções serão:
-
-  1 - Cadastrar Colaborador;
-  2 - Marcar Ponto;
-  3 - Ver Lista de Colaboradores;
-  4 - Ver Lista de Colaboradores Que Ainda Não Marcaram o Ponto;
-  9 - Encerrar;
 */
 
 let listaColaboradores = [];
@@ -79,15 +37,14 @@ class Validacoes {
 
     //verificar se é numero
     ifNumber = (variavelParaValidar) => {
-        if (parseInt(variavelParaValidar) !== null) {
+        if (!isNaN(variavelParaValidar)) {
             return true;
         } else {
-            alert('Apenas numeros são válidos aqui. Por favor, tente novamente.')
             return false;
         }
     }
 
-    // verificar se não é string quando precisa ser string
+    // verificar se é string
     ifString = (variavelParaValidar) => {
         if (variavelParaValidar.toString().toLowerCase() !== variavelParaValidar.toString().toUpperCase()) {
             return true;
@@ -98,7 +55,7 @@ class Validacoes {
 
     //validar dias
     ifDay = (variavelParaValidar) => {
-        if(variavelParaValidar >= 1 && variavelParaValidar <= 31) {
+        if (variavelParaValidar >= 1 && variavelParaValidar <= 31) {
             return true;
         } else {
             return false;
@@ -107,7 +64,7 @@ class Validacoes {
 
     //validar horas
     ifHour = (variavelParaValidar) => {
-        if(variavelParaValidar >= 0 && variavelParaValidar <= 24) {
+        if (variavelParaValidar >= 0 && variavelParaValidar <= 24) {
             return true;
         } else {
             return false;
@@ -158,8 +115,8 @@ do {
                     validacao = false;
                     break;
                 }
-                
-                if(condicao.ifString(nomeColaborador)) {
+
+                if (condicao.ifString(nomeColaborador)) {
                     idContador++;
                     let colaborador = new Colaborador(idContador, nomeColaborador);
                     criarColaborador(colaborador);
@@ -182,7 +139,7 @@ do {
                     validacao = false;
                     break;
                 }
-                if(condicao.ifNumber(idColaborador)){
+                if (condicao.ifNumber(idColaborador) && findColaborador(idColaborador) !== undefined) {
                     let colaborador = findColaborador(idColaborador);
                     let diaASerMarcado = prompt('Digite o dia a ser marcado')
                     if (condicao.ifNull(diaASerMarcado)) {
@@ -190,23 +147,32 @@ do {
                         validacao = false;
                         break;
                     }
-                    let horasASeremMarcadas = prompt('Digite as horas a serem marcadas')
-                    if (condicao.ifNull(horasASeremMarcadas)) {
-                        validacaoMarcarPonto = true;
-                        validacao = false;
-                        break;
+                    if (condicao.ifDay(diaASerMarcado)) {
+                        let horasASeremMarcadas = prompt('Digite as horas a serem marcadas')
+                        if (condicao.ifNull(horasASeremMarcadas)) {
+                            validacaoMarcarPonto = true;
+                            validacao = false;
+                            break;
+                        }
+                        if (condicao.ifHour(horasASeremMarcadas)) {
+                            colaborador.marcarPonto(diaASerMarcado, horasASeremMarcadas)
+                            validacaoMarcarPonto = true;
+                            alert('Ponto marcado com sucesso!')
+                            console.log(listaColaboradores)
+                        } else {
+                            alert('Você digitou um valor inválido para hora (0 a 24h). Por favor, tente novamente.')
+                        }
+                    } else {
+                        alert('Você digitou um valor inválido para dias (1 a 31). Por favor, tente novamente.')
                     }
-                    colaborador.marcarPonto(diaASerMarcado, horasASeremMarcadas)
-                    validacaoMarcarPonto = true;
-                    console.log(listaColaboradores)
                 } else {
-                    alert('Você precisa digitar apenas números aqui.')
+                    alert('Você digitou um valor inválido. Por favor, tente novamente.')
                 }
             } while (!validacaoMarcarPonto)
             break;
 
         case '3':
-            if(listaColaboradores.length === 0) {
+            if (listaColaboradores.length === 0) {
                 alert('Desculpe, não encontramos nenhum dado.')
                 break;
             } else {
@@ -216,7 +182,7 @@ do {
             }
 
         case '4':
-            if(findColaboradoresWithoutPonto().length === 0){
+            if (findColaboradoresWithoutPonto().length === 0) {
                 alert('Desculpe, não encontramos nenhum dado.')
                 break;
             } else {
